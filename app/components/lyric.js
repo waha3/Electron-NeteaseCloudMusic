ce5e254f8eda05839ec08d1651580776b8d3dd49
waha3 <895240740@qq.com>
2017-05-04 00:42:32 +0800

    hanxin

--- app/components/lyric.js
+++ app/components/lyric.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-
+import PropTypes from 'prop-types';
 
 export default class Lyric extends Component {
+  static propTypes = {
+    url: PropTypes.string.isRequired,
+    singer: PropTypes.string.isRequired,
+    albumPic: PropTypes.string.isRequired,
+    songName: PropTypes.string.isRequired,
+    albumName: PropTypes.string.isRequired
+  }
+
   render() {
+    const { albumName, songName, singer } = this.props;
     return (
       <div className="lyric">
         <div className="title">
-          <h1>Hey Jude</h1>
-          <span>MV</span>
-          <span>128K</span>
+          <h2>{songName}<span>128K</span></h2>
         </div>
         <div className="albumdetail">
-          <div>专辑：Ten New Songs</div>
-          <div>歌手： The beatles</div>
+          <div>专辑：{albumName}</div>
+          <div>歌手： {singer}</div>
           <div>来源：沧桑xxx</div>
         </div>
         <div className="lyricdetail">
