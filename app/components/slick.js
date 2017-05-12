8598478a9dfe8c266b6d2b6dcfde7824a9b24144
han <895240740@qq.com>
2017-05-12 18:59:14 +0800

    hanxin

--- app/components/slick.js
+++ app/components/slick.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 
-export default class Slick extends Component {
+class SlickItem extends Component {
   render() {
     return (
+      <div className="slick-item">
+        {}
+      </div>
+    );
+  }
+}
+
+export default class Slick extends Component {
+  state = {
+    activeIndex: 1,
+    items: [1, 2, 3]
+  }
+
+  render() {
+    const { items } = this.state;
+    return (
       <div className="slick">
-        
+        <div className="slick-wrap">
+          {
+            items.map((v, i) => (
+              <SlickItem />
+            ))
+          }
+        </div>
       </div>
     );
   }
